14370d30a533e6ab3e8fe436b0185c4c3e0d8617
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-21 16:41:32 +0530

    updates

--- 02-concurrency/06-communication/13-demo.go
+++ 02-concurrency/06-communication/13-demo.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// consumer
 func main() {
 	ch := make(chan int)
 	go add(100, 200, ch)
@@ -11,6 +12,7 @@ func main() {
 	fmt.Println("result :", result)
 }
 
+// producer
 func add(x, y int, ch chan int) {
 	result := x + y
 	ch <- result
