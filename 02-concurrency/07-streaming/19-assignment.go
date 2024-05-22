74f8cc429786e8f10988c54d10eefd355f9f3630
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-22 10:38:37 +0530

    updates

--- 02-concurrency/07-streaming/19-assignment.go
+++ 02-concurrency/07-streaming/19-assignment.go
@@ -6,10 +6,15 @@ import (
 	"time"
 )
 
+type ResultType struct {
+	No   int
+	Type string
+}
+
 func main() {
 	// Use the same channel to receive data from genPrimes() & genEvenNos() and print them as and ewhen they are generated
 	wg := &sync.WaitGroup{}
-	ch := make(chan int)
+	ch := make(chan ResultType)
 	go func() {
 		wg.Add(1)
 		go genPrimes(2, 100, ch, wg)
@@ -18,16 +23,16 @@ func main() {
 		wg.Wait()
 		close(ch)
 	}()
-	for no := range ch {
-		fmt.Println(no)
+	for result := range ch {
+		fmt.Printf("Type : %q, No : %d\n", result.Type, result.No)
 	}
 }
 
-func genPrimes(start, end int, ch chan<- int, wg *sync.WaitGroup) {
+func genPrimes(start, end int, ch chan<- ResultType, wg *sync.WaitGroup) {
 	defer wg.Done()
 	for no := start; no <= end; no++ {
 		if isPrime(no) {
-			ch <- no
+			ch <- ResultType{No: no, Type: "Prime"}
 			time.Sleep(500 * time.Millisecond)
 		}
 	}
@@ -44,11 +49,11 @@ func isPrime(no int) bool {
 	return true
 }
 
-func genEvenNos(count int, ch chan<- int, wg *sync.WaitGroup) {
+func genEvenNos(count int, ch chan<- ResultType, wg *sync.WaitGroup) {
 	defer wg.Done()
 	// should send one even number (starting from 0) at a time through a channel at 300 ms intervals
 	for i, no := 0, 0; i < count; i, no = i+1, no+2 {
-		ch <- no
+		ch <- ResultType{No: no, Type: "Even"}
 		time.Sleep(300 * time.Millisecond)
 	}
 	fmt.Println("all even number generated")
