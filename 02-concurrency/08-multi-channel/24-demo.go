50c73450bbdcb88980856e4ba0a3c6d6aef0e02f
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-22 11:36:32 +0530

    updates

---
+++ 02-concurrency/08-multi-channel/24-demo.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	stopCh := make(chan struct{})
+
+	ch := genPrimes(stopCh)
+
+	fmt.Println("Hit ENTER to stop...")
+	go func() {
+		fmt.Scanln()
+		close(stopCh)
+	}()
+
+	for primeNo := range ch {
+		fmt.Println("prime no :", primeNo)
+	}
+	fmt.Println("Done")
+}
+
+func genPrimes(stopCh <-chan struct{}) <-chan int {
+	ch := make(chan int)
+	go func() {
+	OUTER_LOOP:
+		for no := 2; ; no++ {
+			if isPrime(no) {
+				select {
+				case <-stopCh:
+					break OUTER_LOOP
+				case ch <- no:
+					time.Sleep(500 * time.Millisecond)
+				}
+			}
+		}
+		fmt.Println("all prime number generated")
+		close(ch)
+	}()
+	return ch
+}
+
+func timeOut(d time.Duration) <-chan time.Time {
+	ch := make(chan time.Time)
+	go func() {
+		time.Sleep(d)
+		close(ch)
+	}()
+	return ch
+}
+
+// utility function to check if a number is prime
+func isPrime(no int) bool {
+	for i := 2; i <= (no / 2); i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
+}
