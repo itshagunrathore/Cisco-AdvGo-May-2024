79273db52581913546ce14952828c42037e9c314
Magesh Kuppan <tkmagesh77@gmail.com>
2024-05-22 10:34:00 +0530

    assignment solution

--- 02-concurrency/07-streaming/19-assignment.go
+++ 02-concurrency/07-streaming/19-assignment.go
@@ -1,11 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 func main() {
 	// Use the same channel to receive data from genPrimes() & genEvenNos() and print them as and ewhen they are generated
+	wg := &sync.WaitGroup{}
+	ch := make(chan int)
+	go func() {
+		wg.Add(1)
+		go genPrimes(2, 100, ch, wg)
+		wg.Add(1)
+		go genEvenNos(60, ch, wg)
+		wg.Wait()
+		close(ch)
+	}()
+	for no := range ch {
+		fmt.Println(no)
+	}
 }
 
-func genPrimes(start, end int) {
-	// should send one prime number at a time through a channel at 500 ms intervals
+func genPrimes(start, end int, ch chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for no := start; no <= end; no++ {
+		if isPrime(no) {
+			ch <- no
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
+	fmt.Println("all prime number generated")
 }
 
 // utility function to check if a number is prime
@@ -18,6 +44,12 @@ func isPrime(no int) bool {
 	return true
 }
 
-func genEvenNos(count int) {
+func genEvenNos(count int, ch chan<- int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	// should send one even number (starting from 0) at a time through a channel at 300 ms intervals
+	for i, no := 0, 0; i < count; i, no = i+1, no+2 {
+		ch <- no
+		time.Sleep(300 * time.Millisecond)
+	}
+	fmt.Println("all even number generated")
 }
